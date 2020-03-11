1711836375a38592a7855de8a898d310b97086df
王一 WangYi <godspeed_china@yeah.net>
2020-03-11 10:13:25 +0800

    Add files via upload

--- model.r
+++ model.r
@@ -8,12 +8,14 @@ for(i in mi:100){
 	vr2[i-mi+1]=cor(x$T,log10(x$N+i))^2;
 	if(vr2[i-mi+1]>model_r2){	model_r2=vr2[i-mi+1];	founders=i;	}
 }
-png("FoundersEstimation.png",width=512,height=512);
+
+png("FoundersEstimation.png",width=768,height=512);
 plot(mi:100,vr2,type='l',xlab="Number of Founders", ylab="R2",	main=paste(name," Founders Estimation"),lwd=2);
-abline(v=fo,col=2,lwd=2);
+abline(v=founders,col=2,lwd=2);
 legend("topright", paste("Founders=",founders));
 dev.off();
-png("Log10Trend.png",width=512,height=512);
+
+png("Log10Trend.png",width=768,height=512);
 plot(x$T,log10(x$N+founders),pch=20,xlab="Day",ylab=paste("log10( N+",founders," )"), main=paste(name," Log10 Scaled Trend"));
 abline(lm(log10(x$N+founders)~x$T),col=2);
 legend("topleft", paste("R2=",sprintf("%.4f",model_r2)));
@@ -30,7 +32,7 @@ for(i in 1:length(t)){
 	else{ t[i]=t[i-1]+1; }
 	n[i]=10^(a*t[i]+b);
 }
-png("InfectionPrediction.png",width=512,height=512);
+png("InfectionPrediction.png",width=768,height=512);
 plot(t,n,type='l',col=2,xlab="Day",ylab="Number of Diagnoses",main=paste(name," Infection Number Prediction"));
 points(x$T,x$N,pch=20);
 legend("topleft", paste("tenfold every ",sprintf("%.1f",1/a), " days"));
